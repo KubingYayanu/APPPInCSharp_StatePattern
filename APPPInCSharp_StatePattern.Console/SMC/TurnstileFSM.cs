dc13a4afebac0869ab787b1f7877e405fc606a6f
Kubing <t6m8@hotmail.com>
2019-04-21 23:04:55 +0800

    add TurnstileFSM

---
+++ APPPInCSharp_StatePattern.Console/SMC/TurnstileFSM.cs
@@ -0,0 +1,32 @@
+﻿namespace APPPInCSharp_StatePattern
+{
+    public class TurnstileFSM : Turnstile
+    {
+        public TurnstileFSM(TurnstileController controller)
+        {
+            this.controller = controller;
+        }
+
+        private readonly TurnstileController controller;
+
+        public override void Lock()
+        {
+            controller.Lock();
+        }
+
+        public override void Unlock()
+        {
+            controller.Unlock();
+        }
+
+        public override void Thankyou()
+        {
+            controller.Thankyou();
+        }
+
+        public override void Alarm()
+        {
+            controller.Alarm();
+        }
+    }
+}
